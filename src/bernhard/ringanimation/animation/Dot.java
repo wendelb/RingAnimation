f9dcaa62780c61bd500a14cb9bf8461c457af5f0
Bernhard Wendel <bwendel@gmx.de>
2015-05-12 11:16:54 +0200

    Kommentare bei der Schweif-Position richtig gestellt

--- src/bernhard/ringanimation/animation/Dot.java
+++ src/bernhard/ringanimation/animation/Dot.java
@@ -85,11 +85,12 @@ public class Dot {
 
 		Polygon p = new Polygon();
 
-		// Der Punkt, der am weitesten weg ist
 		if (directionTowardsCenter) {
+			// Der Punkt der am nächsten dran ist
 			p.addPoint((int) (centerX - Math.cos(Math.toRadians(winkel)) * centerDistance), (int) (centerY - Math.sin(Math.toRadians(winkel)) * centerDistance));
 		}
 		else {
+			// Der Punkt, der am weitesten weg ist
 			p.addPoint((int) (centerX - Math.cos(Math.toRadians(winkel)) * (centerDistance + stepCount * stepSize)), (int) (centerY - Math.sin(Math.toRadians(winkel)) * (centerDistance + stepCount * stepSize)));
 		}
 
