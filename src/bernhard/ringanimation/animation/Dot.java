72e203ab5aad0b5b0e19862d8de45c47d23ab3ea
Bernhard Wendel <bwendel@gmx.de>
2015-05-12 15:57:59 +0200

    Animation umgestellt auf Zeitbasiert statt Tick-basiert

--- src/bernhard/ringanimation/animation/Dot.java
+++ src/bernhard/ringanimation/animation/Dot.java
@@ -8,23 +8,66 @@ public class Dot {
 
 	private float x, y, winkel;
 	private int centerX, centerY;
-	private int step;
 	private float colorStep;
 	private Color color;
-	private boolean directionTowardsCenter;
+	private long lastTime;
 
+	/**
+	 * Minimaler Abstand zum Mittelpunkt
+	 */
 	public static final float centerDistance = 100f;
 
-	public static final int dotRadius = 10;
+	/**
+	 * Radius des einzelnen Puntktes
+	 */
+	public static final int dotRadius = 8;
 
 	// Movement
-	public static final float stepSize = 1.5f;
-	public static final int stepCount = 50;
+	/**
+	 * Bewegungsweite (centerDistance + movingDistance = Maximaler Abstand zum
+	 * Mittelpunkt)
+	 */
+	public static final int movingDistance = 100; // px (wie weit bewegt sich
+													// der Punkt)
 
-	// Color
-	public static final float colorStepSize = 0.01f;
+	/**
+	 * Bewegungsgeschwindigkeit der Animation auf linearbasis Einheit:
+	 * Linearpixel pro Millisekunde Der aktuelle Wert sorgt dafür, dass die
+	 * komplette Animation (raus/rein) in 2 Sekunden abläuft
+	 */
+	public static final double animationSpeed = movingDistance * 2.0 / 1000.0 / 2.0;
 
-	public Dot(int centerX, int centerY, float winkel, int stepPadding) {
+	/**
+	 * Aktueller Schritt, Einheit px; Maximaler Wert: movingDistance; Minimaler
+	 * Wert: 0.0 Verläuft linear, die Werte x und y verlaufen dagegen so, wie es
+	 * die Easing-Funktion verlangt
+	 */
+	private double step;
+
+	/**
+	 * Bewegt sich der Punkt gerade in Richtung Mittelpunkt?
+	 */
+	private boolean directionTowardsCenter;
+
+	// Color Management
+	/**
+	 * Schrittweite, wie viel Grad sich die Farbe auf dem HUE bewegen soll
+	 */
+	public static final float colorStepSize = 0.005f;
+
+	/**
+	 * 
+	 * @param centerX
+	 *            Position des Mittelpunktes
+	 * @param centerY
+	 *            Position des Mittelpunktes
+	 * @param winkel
+	 *            Drehwinkel der Animation in Grad
+	 * @param stepPadding
+	 *            Um wie viel soll sich der Punkt beim Start bereits bewegt
+	 *            haben?
+	 */
+	public Dot(int centerX, int centerY, float winkel, double stepPadding) {
 		this.centerX = centerX;
 		this.centerY = centerY;
 		this.winkel = winkel;
@@ -36,33 +79,38 @@ public class Dot {
 
 		// Sägezahlfunktion, die sich alle 2 * stepCount wiederholt (auf, ab,
 		// auf, ab)
-		stepPadding = stepPadding % (stepCount * 2);
+		while (stepPadding > movingDistance * 2) {
+			// mod-Funktion für Floats
+			stepPadding -= movingDistance * 2;
+		}
 
-		if (stepPadding < stepCount) {
+		if (stepPadding < movingDistance) {
 			this.step = stepPadding;
 		}
 		else {
-			this.step = stepCount - (stepPadding - stepCount);
+			this.step = movingDistance - (stepPadding - movingDistance);
 		}
 
 		updateColor();
 		calcNewPosition();
+
+		lastTime = System.nanoTime();
 	}
 
 	private void calcNewPosition() {
 
-		double p = step / (double) stepCount;
+		double p = step / (double) movingDistance;
 		double factor;
 
 		if (!this.directionTowardsCenter) {
 			// Nach außen
 			p = 1 - p;
-			factor = (centerDistance + stepSize * stepCount - Easing.easeOutExpo(p) * stepSize * stepCount);
+			factor = (centerDistance + movingDistance - Easing.easeOutExpo(p) * movingDistance);
 		}
 		else {
 			// Nach innen
 			// Achtung: Hier gibt es einen anderen Ansatzpunkt
-			factor = (centerDistance + Easing.easeOutExpo(p) * stepSize * stepCount);
+			factor = (centerDistance + Easing.easeOutExpo(p) * movingDistance);
 
 		}
 
@@ -87,12 +135,12 @@ public class Dot {
 
 		if (directionTowardsCenter) {
 			// Ausgehend vom Punkt der am nächsten dran ist hin zum anderen Ende
-			double factor = centerDistance + Easing.easeInOutExpo(this.step / (double) stepCount) * stepCount * stepSize;
+			double factor = centerDistance + Easing.easeInOutExpo(this.step / movingDistance) * movingDistance;
 			p.addPoint((int) (centerX - Math.cos(Math.toRadians(winkel)) * factor), (int) (centerY - Math.sin(Math.toRadians(winkel)) * factor));
 		}
 		else {
 			// Ausgehend vom Punkt, der am weitesten weg ist, nach innen
-			double factor = (centerDistance + stepCount * stepSize) - Easing.easeInOutExpo((stepCount - this.step) / (double) stepCount) * stepCount * stepSize;
+			double factor = (centerDistance + movingDistance) - Easing.easeInOutExpo((movingDistance - this.step) / movingDistance) * movingDistance;
 			p.addPoint((int) (centerX - Math.cos(Math.toRadians(winkel)) * factor), (int) (centerY - Math.sin(Math.toRadians(winkel)) * factor));
 		}
 
@@ -112,17 +160,27 @@ public class Dot {
 		this.color = new Color(Color.HSBtoRGB(colorStep, 1.0f, 1.0f));
 	}
 
+	/**
+	 * Berechnung des neuen Steps und anschließender Berechnung der neuen
+	 * X/Y-Position. Dazu die nächste Farbe
+	 */
 	public void update() {
-		// Aktuellen Schritt berechnen
-		if (this.directionTowardsCenter) {
-			this.step++;
+		long currTime = System.nanoTime();
 
-			if (this.step >= stepCount) {
+		// Zeitliche Differenz zwischen 2 Update-Läufen in ms
+		double diff = (currTime - lastTime) / 1000000.0;
+
+		lastTime = currTime;
+
+		if (this.directionTowardsCenter) {
+			this.step += diff * animationSpeed;
+
+			if (this.step >= movingDistance) {
 				this.directionTowardsCenter = false;
 			}
 		}
 		else {
-			this.step--;
+			this.step -= diff * animationSpeed;
 
 			if (this.step <= 0) {
 				this.directionTowardsCenter = true;
