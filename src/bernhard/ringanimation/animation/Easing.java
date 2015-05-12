b3fe300a69c5fa8cdf1ae302cd5688bbac74d05e
Bernhard Wendel <bwendel@gmx.de>
2015-05-12 11:12:13 +0200

    Easing Klasse weiter ausgebaut

--- src/bernhard/ringanimation/animation/Easing.java
+++ src/bernhard/ringanimation/animation/Easing.java
@@ -1,7 +1,20 @@
 package bernhard.ringanimation.animation;
 
 public class Easing {
-	public static double easeOutExpo(double p) {
-		return 1 - Math.pow(1 - p, 4 + 2);
+	private static double easeExpo(double p) {
+		return Math.pow(p, 4 + 2);
 	}
+
+	public static double easeInExpo(double p) {
+		return easeExpo(p);
+	}
+	
+	public static double easeOutExpo(double p) {
+		return 1 - easeExpo(1 - p);
+	}
+
+	public static double easeInOutExpo(double p) {
+		return p < 0.5 ? easeExpo(p * 2) / 2 : 1 - easeExpo(p * -2 + 2) / 2;
+	}
+
 }
